ee0dfdd170a5fed807dcfdf2b67a4857c0be7f77
KaranAgarwal13 <65128650+KaranAgarwal13@users.noreply.github.com>
2020-05-11 21:56:45 +0530

    Create floodFill.cpp

---
+++ floodFill.cpp
@@ -0,0 +1,17 @@
+void DFS(vector<vector<int>>& image, int sr, int sc, int newColor, int oldColor)
+{
+	if(sr>=0 && sc>=0 && sr<image.size() && sc<image[0].size() && image[sr][sc]==oldColor)
+	{
+		image[sr][sc]=newColor;
+        DFS(image, sr, sc+1, newColor, oldColor);
+        DFS(image, sr+1, sc, newColor, oldColor);
+        DFS(image, sr, sc-1, newColor, oldColor);
+        DFS(image, sr-1, sc, newColor, oldColor);            
+    }
+}
+vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
+{
+    if(newColor!=image[sr][sc])
+        DFS(image, sr, sc, newColor, image[sr][sc]);
+    return image;
+}
