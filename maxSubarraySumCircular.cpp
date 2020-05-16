a9b576f0887af420b688f9458b860a23d2b0ff5d
Karan Agarwal <65128650+KaranAgarwal13@users.noreply.github.com>
2020-05-16 23:30:49 +0530

    Update maxSubarraySumCircular.cpp

--- maxSubarraySumCircular.cpp
+++ maxSubarraySumCircular.cpp
@@ -1,17 +1,19 @@
 int maxSubarraySumCircular(vector<int>& A) {
-	int currSum, maxSum, index = 0;
-    currSum = maxSum = A[0];
-    for(int i = 1; i < 2*A.size(); i++)
-    {
-        //stopping condition for circular array
-        if(A[i%A.size()] > currSum+A[i%A.size()])
-        {
-            currSum = A[i%A.size()];
-            index = i;
+        if(A.size() == 0) return 0;
+        int sum = A[0];
+        int maxSoFar = A[0];
+        int maxTotal = A[0];
+        int minSoFar = A[0];
+        int minTotal = A[0];
+        for(int i = 1; i< A.size();i++){
+            maxSoFar = max(A[i], maxSoFar + A[i]);
+            maxTotal = max(maxTotal, maxSoFar);
+            
+            minSoFar = min(A[i], minSoFar + A[i]);
+            minTotal = min(minTotal, minSoFar);
+            sum += A[i];
+
         }
-        else
-            currSum += A[i%A.size()];
-        maxSum = (currSum > maxSum)? currSum : maxSum;
-    }
-    return maxSum;
+        if(sum == minTotal) return maxTotal;
+        return max(sum - minTotal, maxTotal);
 }
